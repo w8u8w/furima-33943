1c1441615baa093d382412c990596752562b4dc1
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-17 12:17:53 +0900

    バリエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchases_records
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください' 
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'  
+
+  validates :nickname,       presence: true
+  validates :birthday,       presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name_em
+    validates :first_name_em
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
