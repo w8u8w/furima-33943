bcaca181329874860f04f6e9521bca33206733e7
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-17 20:01:53 +0900

    エラーの原因となる不要記述の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください' 
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'  
 
   validates :nickname,       presence: true
   validates :birthday,       presence: true
