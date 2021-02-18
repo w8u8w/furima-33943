de786cf08f7093a0ff839bf5f7644b0359d188d8
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-18 20:02:07 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to  :user
   has_one     :purchases_record
   has_one_attached  :image
+
+  with_options presence: true do
+    validates :name_of_item
+    validates :item_description
+    validates :item_category_id
+    validates :item_condition_id
+    validates :who_expense_id
+    validates :prefecture_id
+    validates :days_to_shipment_id
+    validates :sales_price
+    validates :image
+  end
 end
