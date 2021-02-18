423b058c2d492d7b0dd6277d629fcae92372d1af
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-18 19:34:59 +0900

    画像保存許可のストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,11 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name_of_itme, :item_description, :item_category_id, :item_condition_id, :who_expense_id, :prefecture_id, :days_to_shipment_id, :sales_price, :image).merge(user_id: current_user.id)
+  end
+  end
 end
