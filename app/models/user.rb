312ec0448f0863825472b25f9ce822102ea7d5cc
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-18 17:34:21 +0900

    with_optionsによる記述まとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  validates :nickname,       presence: true
-  validates :birthday,       presence: true
+  with_options presence: true
+    validates :nickname
+    validates :birthday
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_em
