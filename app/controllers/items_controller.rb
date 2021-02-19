772f512842c4017f85834c00e861b910be54711a
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-19 17:14:41 +0900

    スペルミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name_of_itme, :item_description, :item_category_id, :item_condition_id, :who_expense_id, :prefecture_id, :days_to_shipment_id, :sales_price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name_of_item, :item_description, :item_category_id, :item_condition_id, :who_expense_id, :prefecture_id, :days_to_shipment_id, :sales_price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
