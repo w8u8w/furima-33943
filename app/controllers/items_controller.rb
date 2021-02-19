97571f2b2a87bb00ca1c16f87356be39b5651e84
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-19 12:06:52 +0900

    記述ミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,5 +14,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name_of_itme, :item_description, :item_category_id, :item_condition_id, :who_expense_id, :prefecture_id, :days_to_shipment_id, :sales_price, :image).merge(user_id: current_user.id)
   end
-  end
 end
