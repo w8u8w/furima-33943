d6c3e7471deacc69de72e3a08bc157e3e214f3cb
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-19 12:17:00 +0900

    未ログイン状態では、出品ページへ移動せず、ログインページへリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
   def index
   end
 
@@ -13,5 +16,14 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name_of_itme, :item_description, :item_category_id, :item_condition_id, :who_expense_id, :prefecture_id, :days_to_shipment_id, :sales_price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    else
+      current_user == @user
+      redirect_to action: :index
+    end    
   end
 end
