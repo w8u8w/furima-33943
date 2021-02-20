7e8065d459c41f37a8e14c7c9cac4c582bb1794d
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-20 18:10:07 +0900

    販売手数料、販売利益の計算表示を実装

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,6 +2,9 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
- })
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(Number(inputValue) * 0.1);
+    const resultProfit = document.getElementById("profit");
+    resultProfit.innerHTML = Math.floor(Number(inputValue) - Number(addTaxDom.innerHTML))
+  })
 })
