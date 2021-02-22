4677ecfbd35fb130bdc7902f224134712f0e7c80
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-22 14:43:23 +0900

    updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,11 @@ class ItemsController < ApplicationController
 
   def update
     item= Item.find(params[:id])
-    item.update(item_params)
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render  :edit
+    end
   end
 
   private
