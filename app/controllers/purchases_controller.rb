d35f01f46d4f40384be5070af882f2ae05ba9e01
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-23 17:44:14 +0900

    Formオブジェクトのインスタンス生成、インスタンス変数へ代入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,23 @@
 class PurchasesController < ApplicationController
 
   def index
-    
+    @purchases_shipping = PurchasesShipping.new
   end
 
   def create
-    
+    @purchases_shipping = PurchasesShipping.new(purchases_params)
+    if @purchases_shipping.valid?
+      @purchases_shipping.save
+      redirect_to root_path
+    else
+      render  :index
+    end
   end
+
+  private
+
+  def purchases_params
+    params.require(:purchases_record).permit(:post_code, :prefecture_id, :municipality, :block_number, :building_name).merge(user_id: current_user.id)
+  end
+
 end
