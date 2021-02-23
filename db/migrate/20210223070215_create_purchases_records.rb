8a8d51756fbf3226ac1d4ad60731f3b139eed29f
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-23 16:12:30 +0900

    マイグレーションを実行し、テーブルを作成

--- db/migrate/20210223070215_create_purchases_records.rb
+++ db/migrate/20210223070215_create_purchases_records.rb
@@ -1,7 +1,8 @@
 class CreatePurchasesRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases_records do |t|
-
+      t.references	:user,                foreign_key: true
+      t.references	:item,                foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_102053) do
+ActiveRecord::Schema.define(version: 2021_02_23_070243) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,28 @@ ActiveRecord::Schema.define(version: 2021_02_18_102053) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "purchases_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchases_records_on_item_id"
+    t.index ["user_id"], name: "index_purchases_records_on_user_id"
+  end
+
+  create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipality", null: false
+    t.string "block_number", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.bigint "purchases_record_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchases_record_id"], name: "index_shipping_addresses_on_purchases_record_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -68,4 +90,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_102053) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "purchases_records", "items"
+  add_foreign_key "purchases_records", "users"
+  add_foreign_key "shipping_addresses", "purchases_records"
 end
