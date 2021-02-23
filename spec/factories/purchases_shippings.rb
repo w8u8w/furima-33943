5e3c6054908241397899601c239442daffa71b95
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-24 01:01:28 +0900

    配送先情報のテストコードの記述

--- spec/factories/purchases_shippings.rb
+++ spec/factories/purchases_shippings.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchases_shipping do
-    
+    post_code { '123-4567' }
+    prefecture_id { 2 }
+    municipality { '札幌市' }
+    block_number { '札幌' }
+    building_name { 'あいうビル' }
+    phone_number { '12345678901' }
   end
 end
--- spec/models/purchases_shipping_spec.rb
+++ spec/models/purchases_shipping_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe PurchasesShipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchases_shipping = FactoryBot.build(:purchases_shipping, user_id: user.id, item_id: item.id)
+  end
+
+  describe '商品の購入登録' do
+    context '商品の購入成功' do
+      it '入力必須項目の全てが存在すれば購入できる' do
+        expect(@purchases_shipping).to be_valid
+      end
+    end
+
+    context '商品の購入失敗(未入力)' do
+      it '郵便番号がないと購入できない' do
+        @purchases_shipping.post_code = ''
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Post code can't be blank", "Post code is invalid. Include hyphen(-)")
+      end
+      it '都道府県を指定しないと購入できない' do
+        @purchases_shipping.prefecture_id = nil
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it '市区町村がないと購入できない' do
+        @purchases_shipping.municipality = ''
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '番地がないと購入できない' do
+        @purchases_shipping.block_number = ''
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Block number can't be blank")
+      end
+      it '電話番号がないと購入できない' do
+        @purchases_shipping.phone_number = ''
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+      end
+    end
+
+    context '商品の購入失敗（ActiveHash）' do
+      it '都道府県のidが 1 の場合購入できない' do
+        @purchases_shipping.prefecture_id = 1
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+    end
+
+    context '商品の購入失敗（入力不適合）' do
+      it '郵便番号にハイフンがない場合購入できない' do
+        @purchases_shipping.post_code = '1234567'
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it '電話番号が12桁以上の場合購入できない' do
+        @purchases_shipping.phone_number = '123456789012'
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号にハイフンが含まれると登録できない' do
+        @purchases_shipping.phone_number = '123-4567-8901'
+        @purchases_shipping.valid?
+        expect(@purchases_shipping.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+  end
 end
