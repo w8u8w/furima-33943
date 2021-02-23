646765b6d0529d3dc66d0dd0b8b6146d017ce265
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-23 23:15:51 +0900

    コントローラー内の記述ミス、mergeメソッドの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,6 +6,7 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchases_shipping = PurchasesShipping.new(purchases_params)
     if @purchases_shipping.valid?
       @purchases_shipping.save
@@ -18,7 +19,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchases_params
-    params.require(:purchases_record).permit(:post_code, :prefecture_id, :municipality, :block_number, :building_name).merge(user_id: current_user.id)
+    params.require(:purchases_shipping).permit(:post_code, :prefecture_id, :municipality, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
