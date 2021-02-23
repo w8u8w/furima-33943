5792889f8c9bce041cd39320c3c78f559d37154a
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-23 16:00:40 +0900

    ルーティング、コントローラー、ビューを設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,10 @@
 class PurchasesController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
+  resources :purchases, only: :index do
+    resources :purchases, only: :create
+  end
 end
