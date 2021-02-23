eb07347f31a9c92c6ec0fdff868315c3ca2b74e3
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-23 16:15:21 +0900

    モデルにアソシエーションを追加

--- app/models/purchases_record.rb
+++ app/models/purchases_record.rb
@@ -1,2 +1,5 @@
 class PurchasesRecord < ApplicationRecord
+  belongs_to  :user
+  belongs_to  :item
+  has_one     :shipping_address
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,3 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to  :purchases_record
 end
