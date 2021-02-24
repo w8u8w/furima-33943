038f5c2745b5baeda8926e4fc3d769ada044bc19
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-24 12:24:29 +0900

    決済処理の記述をリファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchases_shipping = PurchasesShipping.new(purchases_params)
     if @purchases_shipping.valid?
+      pay_item
       @purchases_shipping.save
       redirect_to root_path
     else
@@ -21,6 +22,15 @@ class PurchasesController < ApplicationController
 
   def purchases_params
     params.require(:purchases_shipping).permit(:post_code, :prefecture_id, :municipality, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_"
+    Payjp::Charge.create(
+      amount: @item.sales_price,
+      card: purchases_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
