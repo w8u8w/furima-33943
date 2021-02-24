277c44a365200f0f68b4eada26ade03e8c5e8bd7
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-24 11:20:21 +0900

    Formオブジェクトで生成した、PpurchasesShippingモデルでtokenを扱えるよう追記

--- app/models/purchases_shipping.rb
+++ app/models/purchases_shipping.rb
@@ -1,6 +1,6 @@
 class PurchasesShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :block_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :block_number, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
