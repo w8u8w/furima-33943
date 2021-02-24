260d77c2bd52b4bd94c0a32cd81873c2ec9affa2
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-02-24 11:12:19 +0900

    クレジットカード情報の削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -22,6 +22,11 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
         debugger;
       }
+
+    document.getElementById("card-number").removeAttribute("name");
+    document.getElementById("card-exp-month").removeAttribute("name");
+    document.getElementById("card-exp_year").removeAttribute("name");
+    document.getElementById("card-cvc").removeAttribute("name");
     });
   })
 };
