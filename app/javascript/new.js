786d2bc4550dd5ffe3f676d2dd19a4c0ca679e1c
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-03-01 19:31:02 +0900

    出品画面にて画像情報の表示

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    // 画像を表示するためのdiv要素を生成
+    const imageElement = document.createElement('div');
+
+    // 表示する画像を生成
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
+})
+
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
