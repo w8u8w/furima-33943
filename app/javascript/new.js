955ee9ae3e4955c049a89b2498c0f0e0aacaed97
w8u8 <on92.hidekata.yusuke@gmail.com>
2021-03-02 18:05:11 +0900

    表示画像の差し替え機能を追加

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
+    // 画像が表示されている場合のみ、すでに存在している画像を削除する
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
